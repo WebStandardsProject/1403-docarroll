81dd63a65173e3942088f241506b0cad07cf7553
Danny O'Carroll <LemonMyth@live.com>
2014-03-20 18:36:29 -0400

    Committing hides

--- projects/js/main.js
+++ projects/js/main.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 $('.searchBox').hide();
-$( ".searchBtn" ).click(function() {
+$('.loginBox').hide();
+$('.detailsBox').hide();	
+$
+( ".searchBtn" ).click(function() {
   $( ".searchBox" ).slideToggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+$( ".loginBtn" ).click(function() {
+  $( ".loginBox" ).slideToggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+$( ".deatilsBtn" ).click(function() {
+  $( ".detailsBox" ).slideToggle( "slow", function() {
     // Animation complete.
   });
 });
